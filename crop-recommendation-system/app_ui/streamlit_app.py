e7ea75ffb2e5385d3b8f35e70d763fbf593dc97d
Jayachandran J <jayachandranjaya82@gmail.com>
2026-03-22 20:19:18 +0530

    Removed virtual environment from tracking

--- crop-recommendation-system/app_ui/streamlit_app.py
+++ crop-recommendation-system/app_ui/streamlit_app.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 st.set_page_config(page_title="Smart Crop AI", layout="centered")
 
-st.title("🌾 Smart Crop Recommendation (AI Powered)")
+st.title("Smart Crop Recommendation System ")
 
-st.subheader("🌱 Soil & Weather Inputs")
+st.subheader("Soil & Weather Inputs")
 
 # Inputs
 N = st.number_input("Nitrogen (N)", 0, 150, 50)
@@ -14,12 +14,12 @@ P = st.number_input("Phosphorus (P)", 0, 150, 50)
 K = st.number_input("Potassium (K)", 0, 150, 50)
 
 temperature = st.slider("🌡 Temperature (°C)", 10, 45, 25)
-humidity = st.slider("💧 Humidity (%)", 10, 100, 60)
-ph = st.slider("⚗️ Soil pH", 3.5, 9.0, 6.5)
+humidity = st.slider("Humidity (%)", 10, 100, 60)
+ph = st.slider("Soil pH", 3.5, 9.0, 6.5)
 rainfall = st.slider("🌧 Rainfall (mm)", 0, 3000, 600)
 
 # Predict
-if st.button("🚀 Predict Crop"):
+if st.button("Predict Crop"):
     try:
         res = requests.get(
             "http://127.0.0.1:8000/predict",
@@ -36,9 +36,9 @@ if st.button("🚀 Predict Crop"):
 
         data = res.json()
 
-        st.success(f"🌱 Best Crop: {data['prediction']}")
+        st.success(f"Best Crop: {data['prediction']}")
 
-        st.subheader("🏆 Top 3 Recommendations")
+        st.subheader("Top 3 Recommendations")
 
         df = pd.DataFrame(data["top3"])
         st.bar_chart(df.set_index("crop"))
